764d4b5b2d6ec0a2e581c927fb3723d200b2790f
isaaacOliveira <pedrohokaoliveira@gmail.com>
2026-06-21 13:06:52 +0100

    fix:instalar nodejs e npm nativos do alpine

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,28 +1,9 @@
-<?php
+ <?php
 
-namespace App\Providers;
+use App\Providers\AppServiceProvider;
+use App\Providers\FortifyServiceProvider;
 
-use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL; // <-- Garante que esta linha está aqui no topo
-
-class AppServiceProvider extends ServiceProvider
-{
-/**
-* Register any application services.
-*/
-public function register(): void
-{
-//
-}
-
-/**
-* Bootstrap any application services.
-*/
-public function boot(): void
-{
-// Se o site estiver no Render (produção), força o HTTPS
-if (config('app.env') === 'production' || isset($_SERVER['HTTPS'])) {
-URL::forceScheme('https');
-}
-}
-}
+return [
+    AppServiceProvider::class,
+    FortifyServiceProvider::class,
+];
