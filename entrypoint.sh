f82668dc46688c8990a942e9968125ce29171bb8
isaaacOliveira <pedrohokaoliveira@gmail.com>
2026-06-21 14:57:47 +0100

    feat:força execucao isolada do seeder para  admin

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,10 +13,9 @@ npm run build
 
 # O --force é obrigatório em ambiente de produção
 php artisan migrate --force
+
 #executar o seeder para te dar permissoes de admin 
 php artisan db:seed --force
-# Rodar as migrations automaticamente (importante para o laravel-permission)
-
 
 
 # Iniciar o PHP-FPM em segundo plano e o Nginx em primeiro plano
